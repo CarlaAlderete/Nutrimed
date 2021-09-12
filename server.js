ed0e8638b4d1a3d53a9574363040e4f28afd4264
Carla <carlamalderete@gmail.com>
2021-09-12 18:10:35 -0300

    final

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const router = require("./routes/index");
 require("./config/database");
 require("./config/passport");
 const socket = require("socket.io");
+const path = require('path')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname + "/client/build/index.html"))
+  })
+}
 
-const server = app.listen(4000, () =>
+const server = app.listen(process.env.PORT || 4000, process.env.MYHOST || '0.0.0.0', () =>
   console.log("Server listening on port 4000")
 );
 
