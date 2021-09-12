ea5058f98df81cb505ec8d225b4011ef6abe2309
Carla <carlamalderete@gmail.com>
2021-09-12 18:54:10 -0300

    nutriMed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ const App = ({ logWithLs, getSocket, user, valid }) => {
    const [socket, setSocket] = useState(null)
 
    useEffect(() => {
-      setSocket(io("http://localhost:4000/"))
+      setSocket(io("https://nutrimed.herokuapp.com/"))
       if (localStorage.getItem("token")) {
          logWithLs(localStorage.getItem("token"))
       }
